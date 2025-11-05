21d8994feb11bf3a5d37b964349bed64da4e97a6
Snapps <snapples64@gmail.com>
2025-11-05 14:29:44 -0800

    requests - requests can now be made

--- src/requests.js
+++ src/requests.js
@@ -1,31 +1,69 @@
 import { env } from "cloudflare:workers";
+import * as  users from "./users";
 import * as  messanger from "./messenger" ;
+import * as database from "./database"
 
 // create request
 export async function createRequest(requestData) {
 
-    // default payload
+    // default payloads
     var payload = {status:null,data:null}
 
-
+    var user = await (await users.getUserByUUID(requestData.owner)).data[0]
+    
+    // check if user is banned
+    if (users.isBanned(user)) {
+        payload.status = "error"
+        payload.data = "user is banned"
+        return payload
+    }
+        
     // check if user is verified
+    if (!users.isVerified(user)) {
+        payload.status = "error"
+        payload.data = "user not verified"
+        return payload
+    }
+    
     // check if user has request permission
+    if (!users.canMakeRequests(user)) {
+        payload.status = "error"
+        payload.data = "user not allowed to make requests"
+        return payload
+    }
 
     // requestData should include...
     // - UUID of requests owner
     // - details of the request
     // - wether the request is (0) public, (1) anonymous, or (2) private
     // - attached reference images
+    
+    // constuct the request
+    var request = {
+        owner:requestData.owner,
+        details: requestData.details,
+        publicity: requestData.publicity,
+        handles: []
+    }
 
     // set UUID
+    request.id = crypto.randomUUID()
     // set IAT
+    request.iat = Date.now()
     // set status to 0
+    request.stats = 0
 
     // add request to Request DB
-    // upload images to Request /reference Bucket
+    const query = `INSERT INTO "main"."requests" ("id", "owner", "details", "has_references", "publicity", "handles", "iat", "status", "finish_time", "finish_product") VALUES('${request.id}', '${request.owner}', '${request.details}', 0, ${request.publicity}, '${JSON.stringify(request.handles)}', ${request.iat}, ${request.stats}, ${null},  ${null}) RETURNING rowid, *`
+    // upload images to Request /reference Bucket`
+    const result = await database.runQuery("SNAPPS_DEV_DB",query)
+
+    //constuct payload
+    payload.data=request
+    payload.status = "ok"
 
     // notify me that a request has been made
-    await messanger.sendToDiscordWebhook();
+    await messanger.sendToDiscordWebhook(env.DISCORD_WEBHOOK_TEST_CHAT,{content:`You got a new request!\n\`\`\`json\n${JSON.stringify(request)}\`\`\``});
     
     // return payload
     return payload
