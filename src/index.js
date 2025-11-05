5ac592be7f2462a55e2864a86e32d5e30e0a9c4a
Snapps <snapples64@gmail.com>
2025-11-04 21:08:04 -0800

    test area

--- src/index.js
+++ src/index.js
@@ -13,8 +13,10 @@ import * as  messanger from "./messenger" ;
 
 export default class extends WorkerEntrypoint
 {
-	async fetch() {
-		await messanger.sendToDiscordWebhook("https://discord.com/api/webhooks/1435425166829293730/SWNdTKdBRvgoLbRL7ST9c09F2VXVgmnvcck8IotlfUOV9anYPIHm74AO_FxVCdvgou8B",{content:"woah"});
-		return new Response('Welcome to snapps.dev!');
+	async fetch(request, env, ctx) {
+		// === Test Area ===
+
+		// =================
+		return new Response("welcome to snapps.dev");
 	}
 };
