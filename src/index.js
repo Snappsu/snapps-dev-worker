f31b283a6ef09fc081782ae75aec9e7f317bbdc8
Snapps <snapples64@gmail.com>
2025-11-04 18:55:54 -0800

    initial set up

--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+// -------------------------
+// --- snapps.dev.worker ---
+// -------------------------
 
-export default {
-	async fetch(request, env, ctx) {
-		return new Response('Hello World!');
-	},
+// This is it, the bulk of what makes snapps.dev what it is.
+// (When I'm done that is)
+
+// This work will handle all of the internal function under the hood of snapps.dev
+
+import { WorkerEntrypoint } from "cloudflare:workers";
+
+import * as  messanger from "./messenger" ;
+
+export default class extends WorkerEntrypoint
+{
+	async fetch() {
+		await messanger.sendToDiscordWebhook("https://discord.com/api/webhooks/1435425166829293730/SWNdTKdBRvgoLbRL7ST9c09F2VXVgmnvcck8IotlfUOV9anYPIHm74AO_FxVCdvgou8B",{content:"woah"});
+		return new Response('Welcome to snapps.dev!');
+	}
 };
